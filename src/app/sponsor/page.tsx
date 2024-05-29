3beff215424d4f65a1341e75d7010f10841dade4
OPstriker <op@gdscout.me>
2024-05-29 08:59:55 -0400

    Add sponsors

--- src/app/sponsor/page.tsx
+++ src/app/sponsor/page.tsx
@@ -1,18 +1,32 @@
-import React from 'react'
+import React from "react";
 
 export default function sponsor() {
   return (
-    <div className="flex flex-col space-y-4">
-      <h2 className="text-3xl">Our Sponsors</h2>
-      <p>
-        The Golden Dragons are grateful for the support of our sponsors. Their
-        generosity helps us to continue to compete in the FIRST Tech Challenge
-        and provide students with hands-on experience in robotics and engineering.
-      </p>
+    <div className="w-full flex flex-col space-y-4">
+      <div className="content flex-col justify-center">
+        <h2 className="text-3xl mt-5 flex justify-center">Our Sponsors</h2>
+        <div className="desc w-full flex justify-center">
+          <p className=" w-96 text-center mt-4">
+            We are grateful for the support of our sponsors since
+            their generosity helps us to continue to compete in FTC (FIRST Tech
+            Challenge) and provide students with hands-on experience using robotics
+            and engineering.
+          </p>
+        </div>
+      </div>
       <div className="list">
-        <li className="gd">Golden Dragons</li>
-        <li className="gssm">GSSM Foundation</li>
+        <li className="gssm flex justify-center space-x-20 mt-5">
+          <a href="https://scgssm.org" target="_blank">
+            <img src="/gssm.png" className="bg-white h-32 p-7 rounded-xl" />
+          </a>
+          <a href="https://www.scgssm.org/who-we-are/gssm-foundation" target="_blank">
+            <img src="/foundation.png" className="h-32 bg-white rounded-xl px-5 py-4" />
+          </a>
+          <a href="https://ed.sc.gov/" target="_blank">
+            <img src="/department.png" className="h-32 bg-white rounded-xl" />
+          </a>
+        </li>
       </div>
     </div>
-  )
+  );
 }
