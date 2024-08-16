4ce51c1413bd4ada311944ec6c5727b260af5803
OPstriker <op@optr.tech>
2024-08-16 15:25:11 -0400

    Updated to current team members. TY for all previous team members!

--- src/components/Team/teamData.tsx
+++ src/components/Team/teamData.tsx
@@ -2,40 +2,16 @@ import { Team } from "@/types/team";
 
 const teamData: Team[] = [
   {
-    id: 12,
-    image: "jay.png",
-    name: "Jaylin Gist",
-    role: ["Team Captain", "Mechanical"]
-  },
-  {
     id: 7,
     image: "gabe.png",
     name: "Gabriel Howe",
     role: ["Software Captain", "Scouting Captain", "Port/Outreach", "Drive/Strategy"]
   },
   {
-    id: 8,
-    image: "ivey.png",
-    name: "Christopher Ivey",
-    role: ["Drive Captain", "Drive Coach/Strategy"]
-  },
-  {
-    id: 13,
-    image: "alayna.png",
-    name: "Alayna Khoja",
-    role: ["Designer", "Port/Outreach"]
-  },
-  {
     id: 1,
     image: "braeden.png",
     name: "Braeden Montroy",
     role: ["Team Captain", "Mechanical Captain", "Scouting Captain"]
-  },
-  {
-    id: 10,
-    image: "nick.png",
-    name: "Niccolo Nisato",
-    role: ["Mechanical", "Backup Driver"]
   },
   {
     id: 3,
@@ -48,12 +24,6 @@ const teamData: Team[] = [
     image: "shivali.png",
     name: "Shivali Patel",
     role: ["Portfolio Captain", "Outreach Captain", "Scouting"]
-  },
-  {
-    id: 9,
-    image: "amolik.png",
-    name: "Amolik Sinha",
-    role: ["Drive Team"]
   },
   {
     id: 4,
@@ -78,12 +48,6 @@ const teamData: Team[] = [
     image: "peyton.png",
     name: "Peyton Weidner",
     role: ["Scouting Captain", "Mechanical", "Electrical", "Port/Outreach", "Software"]
-  },
-  {
-    id: 11,
-    image: "renan.png",
-    name: "Renan Yilmaz",
-    role: ["Mechanical"]
   }
 ]
 
