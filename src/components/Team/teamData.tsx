aeb83ae826f09d862c80f0931316e125b9893596
peiwei <168493939+1peiwei@users.noreply.github.com>
2024-11-06 09:27:43 -0500

    added new people
    added the juniors and moved people around

--- src/components/Team/teamData.tsx
+++ src/components/Team/teamData.tsx
@@ -3,21 +3,21 @@ import { Team } from "@/types/team";
 const teamData: Team[] = [
   {
     id: 1,
-    image: "gabe.png",
-    name: "Gabriel Howe",
-    role: ["Software Captain", "Scouting Captain", "Port/Outreach", "Drive/Strategy"]
-  },
-  {
-    id: 2,
     image: "braeden.png",
     name: "Braeden Montroy",
     role: ["Team Captain", "Mechanical Captain", "Scouting Captain"]
   },
   {
+    id: 2,
+    image: "sanjith.png",
+    name: "Sanjith Sunil",
+    role: ["Software Captain", "Scouting", "Electrical", "Web Dev ❤️"]
+  },
+  {
     id: 3,
-    image: "rishi.png",
-    name: "Rishi Patel",
-    role: ["Drive Captain", "Software"]
+    image: "gabe.png",
+    name: "Gabriel Howe",
+    role: ["Software Captain", "Scouting Captain", "Port/Outreach", "Drive/Strategy"]
   },
   {
     id: 4,
@@ -27,21 +27,57 @@ const teamData: Team[] = [
   },
   {
     id: 5,
-    image: "sanjith.png",
-    name: "Sanjith Sunil",
-    role: ["Software Captain", "Scouting", "Electrical", "web dev ❤️"]
+    image: "rishi.png",
+    name: "Rishi Patel",
+    role: ["Drive Captain", "Software"]
   },
   {
     id: 7,
-    image: "venus.png",
-    name: "Venus",
-    role: ["Outreach", "Drive/Strategy", ]
-  },
-  {
-    id: 8,
     image: "peyton.png",
     name: "Peyton Weidner",
     role: ["Scouting Captain", "Mechanical", "Electrical", "Port/Outreach", "Software"]
+  },
+  {
+    id: 8,
+    image: "venus.png",
+    name: "Venus",
+    role: ["Outreach", "Drive/Strategy"]
+  },
+  {
+    id: 9,
+    image: "albert.png",
+    name: "Albert Mathisz",
+    role: ["Software", "Mechanical", "Outreach"]
+  },
+  {
+    id: 10,
+    image: "griffin.jpeg",
+    name: "Griffin LaRue",
+    role: ["Mechanical", "Port/Outreach", "Drive/Strategy"]
+  },
+  {
+    id: 11,
+    image: "vyom.jpeg",
+    name: "Vyom Madeyanda",
+    role: ["Mechanical", "Outreach", "Electrical"]
+  },
+  {
+    id: 12,
+    image: "carolinel.jpeg",
+    name: "Caroline Locklair",
+    role: ["Outreach/Portfolio", "Electrical", "Software"]
+  },
+  {
+    id: 13,
+    image: "carolinep.jpeg",
+    name: "Caroline Pringle",
+    role: ["Software", "Outreach/Portfolio"]
+  },
+  {
+    id: 14,
+    image: "crystal.png",
+    name: "Crystal Nguyen",
+    role: ["Outreach/Portfolio"]
   }
 ]
 
